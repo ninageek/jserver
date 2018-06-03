7865d1747bd9aeaab9c969eb57b418db6242326c
nina <icefrog911@yahoo.com>
2018-06-02 19:25:48 -0500

    adjust spring boot to deploy to tomcat

--- src/main/java/com/ninageek/application/Application.java
+++ src/main/java/com/ninageek/application/Application.java
@@ -2,13 +2,20 @@ package com.ninageek.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ninageek"} )
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args)throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
